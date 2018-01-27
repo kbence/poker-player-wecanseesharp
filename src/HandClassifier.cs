6facab1df14ba4b2dee74e9048a0288e00e66522
Bence Kiglics <bence.kiglics@gmail.com>
2018-01-27 14:55:02 +0100

    Added missing cs

---
+++ src/HandClassifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Nancy.Simple {
+	public class Hand {
+		public Card[] HoleCards;
+		public Card[] CommunityCards;
+
+		public Hand(Card[] holeCards, Card[] communityCards) {
+			this.HoleCards = holeCards;
+			this.CommunityCards = communityCards;
+		}
+
+		public Card[] AllCards {
+			get {
+				Card[] cards = new Card[HoleCards.Length + CommunityCards.Length];
+				HoleCards.CopyTo(cards, 0);
+				CommunityCards.CopyTo(cards, HoleCards.Length);
+
+				return cards;
+			}
+		}
+	}
+
+	public class HandClassifier {
+		public HandClassifier() {
+		}
+
+		public void Classify(Hand hand) {}
+	}
+}
+
