10d72e95a6d3b10df8410b0865b9e3ba4cf51013
Bence Kiglics <bence.kiglics@gmail.com>
2018-01-27 12:12:06 +0100

    Factored out JSON model

---
+++ src/JsonModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Nancy.Simple {
+	public class Card {
+		public static readonly string[] RANKS = { "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A" };
+
+		public string rank;
+		public string suit;
+
+		public override string ToString() {
+			return String.Format("[Card {0} {1}]", this.suit, this.rank);
+		}
+
+		public int Value {
+			get {
+				return Array.IndexOf(RANKS, rank);
+			}
+		}
+	}
+
+	public class Player {
+		public int id;
+		public string name;
+		public string status;
+		public string version;
+		public int stack;
+		public int bet;
+		public Card[] hole_cards;
+	}
+
+	public class GameObject {
+		public int round;
+		public int bet_index;
+		public int small_blind;
+		public int current_buy_in;
+		public int pot;
+		public int minimum_raise;
+		public int dealer;
+		public int orbits;
+		public int in_action;
+		public Player[] players;
+		public Card[] community_cards;
+	}
+}
+
