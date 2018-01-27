380284ecf1cc400df0477a19c0691e1ccc6d4a88
BalazsVekony <vbalu987@yahoo.co.uk>
2018-01-27 12:21:49 +0100

    Add nUnit and First test

---
+++ src/TestPoker.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    [TestFixture]
+    class TestPoker
+    {
+        [Test]
+        public void TestCards()
+        {
+            Assert.True(true);
+        }
+    }
+}
